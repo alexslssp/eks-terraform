fea273f99c2e31c7e17138edf1e0695fc0ca642f
Alex Silva <alex.silva@quintoandar.com.br>
2024-05-17 10:04:35 -0300

    Update keypair.tf

--- modules/ec2/keypair.tf
+++ modules/ec2/keypair.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "bastion_ssh" {
   key_name   = "${var.project_name}-keypair"
-  public_key = file("~/.ssh/jenkins_id_rsa.pub")
+  public_key = file("~/.ssh/id_rsa")
   tags       = var.tags
 }
